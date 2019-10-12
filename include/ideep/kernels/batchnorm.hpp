11627163897606e4861cb35cbfc2aa0b5414151a
pinzhenx <pinzhen.xu@intel.com>
2019-10-12 13:44:25 +0800

    support bn

--- include/ideep/kernels/batchnorm.hpp
+++ include/ideep/kernels/batchnorm.hpp
@@ -7,15 +7,63 @@ namespace ideep {
 
 struct batch_normalization_forward_inference
     : public dnnl::batch_normalization_forward {
-  static void compute(const tensor& src, const tensor& scale,
-                      const tensor& shift, tensor& dst, float epsilon) {
-    
+  static void compute(const tensor& src,
+                      const tensor& scale,
+                      const tensor& shift,
+                      tensor& dst,
+                      float epsilon,
+                      const engine& aengine = engine::cpu_engine()) {
+    // XPZ: this overload has not been tested
+    auto src_desc = src.get_desc();
+
+    auto flags = batch_normalization_flag::use_scale_shift;
+    // XPZ: TODO: attr?
+    auto pd = dnnl::batch_normalization_forward::primitive_desc(
+        {prop_kind::forward_inference, src_desc, epsilon, flags},
+        aengine);
+
+    tensor scale_shift {pd.weights_desc()};
+    std::memcpy(scale_shift.get_data_handle(),
+                scale.get_data_handle(), scale.get_size());
+    std::memcpy(scale_shift.get_data_handle() + scale.get_size(),
+                shift.get_data_handle(), shift.get_size());
+
+    dnnl::batch_normalization_forward(pd).execute(stream::default_stream(),
+                                          {{DNNL_ARG_SRC, src},
+                                           {DNNL_ARG_SCALE_SHIFT, scale_shift},
+                                           {DNNL_ARG_DST, dst}});
   }
 
-  static void compute(const tensor& src, const tensor& mean,
-                      const tensor& variance, const tensor& scale,
-                      const tensor& shift, tensor& dst, float epsilon) {
+  static void compute(const tensor& src,
+                      const tensor& mean,
+                      const tensor& variance,
+                      const tensor& scale,
+                      const tensor& shift,
+                      tensor& dst,
+                      float epsilon,
+                      const engine& aengine = engine::cpu_engine()) {
+    auto src_desc = src.get_desc();
+    dst.reinit_if_necessary(src_desc);
 
+    auto flags = batch_normalization_flag::use_scale_shift |
+                 batch_normalization_flag::use_global_stats;
+    // XPZ: TODO: attr?
+    auto pd = dnnl::batch_normalization_forward::primitive_desc(
+        {prop_kind::forward_inference, src_desc, epsilon, flags},
+        aengine);
+
+    tensor scale_shift {pd.weights_desc()};
+    std::memcpy(scale_shift.get_data_handle(),
+                scale.get_data_handle(), scale.get_size());
+    std::memcpy(scale_shift.get_data_handle() + scale.get_size(),
+                shift.get_data_handle(), shift.get_size());
+
+    dnnl::batch_normalization_forward(pd).execute(stream::default_stream(),
+                                          {{DNNL_ARG_SRC, src},
+                                           {DNNL_ARG_SCALE_SHIFT, scale_shift},
+                                           {DNNL_ARG_VARIANCE, variance},
+                                           {DNNL_ARG_MEAN, mean},
+                                           {DNNL_ARG_DST, dst}});
   }
 };
 
