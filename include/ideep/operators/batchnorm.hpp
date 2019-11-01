af66a0a9ac6ccf9f33277080a65d021b25c36c89
xiaobing.zhang <xiaobing.zhang@intel.com>
2019-11-01 12:50:00 +0800

    Add batchnorm backward

--- include/ideep/operators/batchnorm.hpp
+++ include/ideep/operators/batchnorm.hpp
@@ -1,5 +1,6 @@
 #ifndef IDEEP_OPERATORS_BATCHNORM_HPP
 #define IDEEP_OPERATORS_BATCHNORM_HPP
+#include "sum.hpp"
 
 namespace ideep {
 
@@ -17,22 +18,20 @@ struct batch_normalization_forward_inference
                       const engine& aengine = engine::cpu_engine()) {
     // XPZ: this overload has not been tested
     auto flags = batch_normalization_flag::use_scale_shift;
-
     auto src_desc = src.get_desc();
-    dst.reinit_if_necessary(src_desc);
-
     // XPZ: TODO: attr?
     auto pd = primitive_desc(
         {prop_kind::forward_inference, src_desc, epsilon, flags}, aengine);
-
+    // have problem using pd.weights_desc?
     tensor scale_shift {pd.weights_desc()};
     std::memcpy(scale_shift.get_data_handle(),
                 scale.get_data_handle(), scale.get_size());
     std::memcpy(scale_shift.get_data_handle() + scale.get_size(),
                 shift.get_data_handle(), shift.get_size());
-
+    auto expected_src = src.reorder_if_necessary(pd.src_desc());
+    dst.reinit_if_necessary(pd.dst_desc());
     super(pd).execute(stream::default_stream(),
-                      {{DNNL_ARG_SRC, src},
+                      {{DNNL_ARG_SRC, expected_src},
                        {DNNL_ARG_SCALE_SHIFT, scale_shift},
                        {DNNL_ARG_DST, dst}});
   }
@@ -45,34 +44,33 @@ struct batch_normalization_forward_inference
                       tensor& dst,
                       float epsilon,
                       const engine& aengine = engine::cpu_engine()) {
-
     auto flags = batch_normalization_flag::use_scale_shift |
                  batch_normalization_flag::use_global_stats;
-
     auto src_desc = src.get_desc();
-    dst.reinit_if_necessary(src_desc);
-
     // XPZ: TODO: attr?
     auto pd = primitive_desc(
         {prop_kind::forward_inference, src_desc, epsilon, flags}, aengine);
-
     tensor scale_shift {pd.weights_desc()};
     std::memcpy(scale_shift.get_data_handle(),
                 scale.get_data_handle(), scale.get_size());
     std::memcpy(scale_shift.get_data_handle() + scale.get_size(),
                 shift.get_data_handle(), shift.get_size());
-
+    auto expected_src = src.reorder_if_necessary(pd.src_desc());
+    auto expected_mean = mean.reorder_if_necessary(pd.mean_desc());
+    auto expected_variance = variance.reorder_if_necessary(pd.variance_desc());
+    dst.reinit_if_necessary(pd.dst_desc());
     super(pd).execute(stream::default_stream(),
-                      {{DNNL_ARG_SRC, src},
+                      {{DNNL_ARG_SRC, expected_src},
                        {DNNL_ARG_SCALE_SHIFT, scale_shift},
-                       {DNNL_ARG_VARIANCE, variance},
-                       {DNNL_ARG_MEAN, mean},
+                       {DNNL_ARG_VARIANCE, expected_variance},
+                       {DNNL_ARG_MEAN, expected_mean},
                        {DNNL_ARG_DST, dst}});
   }
 };
 
 struct batch_normalization_forward_training
     : public dnnl::batch_normalization_forward {
+  using super = dnnl::batch_normalization_forward;
   float get_epsilon() const { return 0.f; }
 
   // batch_normalization_forward_training(const tensor::desc& src_desc, const
@@ -87,31 +85,105 @@ struct batch_normalization_forward_training
 
   tensor::desc expected_statistic_descriptor() const { return tensor::desc(); }
 
-  static void compute(const tensor& src, const tensor& scale,
-                      const tensor& shift, tensor& dst, tensor& mean,
-                      tensor& variance, float momentum, float epsilon) {}
+  static void compute(const tensor& src,
+                      const tensor& scale,
+                      const tensor& shift,
+                      tensor& dst,
+                      tensor& mean,
+                      tensor& variance,
+                      float momentum,
+                      float epsilon,
+                      const engine& aengine = engine::cpu_engine()) {
+    auto flags = batch_normalization_flag::use_scale_shift;
+    auto src_desc = src.get_desc();
+    // XPZ: TODO: attr?
+    auto pd = primitive_desc(
+        {prop_kind::forward_training, src_desc, epsilon, flags}, aengine);
+    tensor scale_shift {pd.weights_desc()};
+    std::memcpy(scale_shift.get_data_handle(),
+                scale.get_data_handle(), scale.get_size());
+    std::memcpy(scale_shift.get_data_handle() + scale.get_size(),
+                shift.get_data_handle(), shift.get_size());
+    auto expected_src = src.reorder_if_necessary(pd.src_desc());
+    mean.reinit_if_necessary(pd.mean_desc());
+    variance.reinit_if_necessary(pd.variance_desc());
+    dst.reinit_if_necessary(pd.dst_desc());
+    super(pd).execute(stream::default_stream(),
+                      {{DNNL_ARG_SRC, expected_src},
+                       {DNNL_ARG_SCALE_SHIFT, scale_shift},
+                       {DNNL_ARG_MEAN, mean},
+                       {DNNL_ARG_VARIANCE, variance},
+                       {DNNL_ARG_DST, dst}});
+  }
 
   static void compute(const tensor& src, const tensor& scale,
                       const tensor& shift, tensor& dst, tensor& mean,
                       tensor& variance, tensor& running_mean,
-                      tensor& running_var, float momentum, float epsilon) {}
+                      tensor& running_var, float momentum, float epsilon) {
+   compute(src, scale, shift, dst, mean, variance, momentum, epsilon);
+   ideep::sum::compute({1 - momentum, momentum}, {running_mean, mean}, running_mean);
+   ideep::sum::compute({1 - momentum, momentum}, {running_var, variance}, running_var);
+  }
 };
 
 struct batch_normalization_backward
     : public dnnl::batch_normalization_backward {
+  using super = dnnl::batch_normalization_backward;
   float get_epsilon() const { return 0.f; }
 
-  prop_kind get_prop_kind() const { return prop_kind::forward; }
+  prop_kind get_prop_kind() const { return prop_kind::backward; }
 
-  static void compute(const tensor& src, const tensor& mean,
-                      const tensor& variance, const tensor& grady,
-                      const tensor& scale, tensor& gradx, tensor& gradw,
-                      float epsilon) {}
+  static void compute(const tensor& src,
+                      const tensor& mean,
+                      const tensor& variance,
+                      const tensor& diff_dst,
+                      const tensor& scale,
+                      tensor& diff_src,
+                      tensor& diff_scale_shift,
+                      float epsilon,
+                      const engine& aengine = engine::cpu_engine()) {
+    // TODO: support no-affine model
+    auto flags = batch_normalization_flag::use_scale_shift;
+    auto src_desc = src.get_desc();
+    auto forward_hints = dnnl::batch_normalization_forward::primitive_desc(
+        {prop_kind::forward_training, src_desc, epsilon, flags}, aengine);
+    auto diff_src_desc = diff_dst.get_desc();
+    auto pd = primitive_desc(
+        {prop_kind::backward, diff_src_desc, src_desc, epsilon, flags}, aengine, forward_hints);
+    auto expected_diff_dst = diff_dst.reorder_if_necessary(pd.diff_dst_desc());
+    auto expected_src = src.reorder_if_necessary(pd.src_desc());
+    auto expected_mean = mean.reorder_if_necessary(pd.mean_desc());
+    auto expected_variance = variance.reorder_if_necessary(pd.variance_desc());
+    diff_src.reinit_if_necessary(pd.diff_src_desc());
+    diff_scale_shift.reinit_if_necessary(pd.diff_weights_desc());
+    super(pd).execute(stream::default_stream(),
+                      {{DNNL_ARG_SRC, expected_src},
+                      {DNNL_ARG_DIFF_DST, expected_diff_dst},
+                      {DNNL_ARG_SCALE_SHIFT, scale}, // only need scale
+                      {DNNL_ARG_MEAN, expected_mean},
+                      {DNNL_ARG_VARIANCE, expected_variance},
+                      {DNNL_ARG_DIFF_SRC, diff_src},
+                      {DNNL_ARG_DIFF_SCALE_SHIFT, diff_scale_shift}});   
+  }
 
-  static void compute(const tensor& src, const tensor& mean,
-                      const tensor& variance, const tensor& grady,
-                      const tensor& scale, tensor& gradx, tensor& grad_scale,
-                      tensor& grad_shift, float epsilon) {}
+  static void compute(const tensor& src,
+                      const tensor& mean,
+                      const tensor& variance,
+                      const tensor& diff_dst,
+                      const tensor& scale,
+                      tensor& diff_src,
+                      tensor& diff_scale,
+                      tensor& diff_shift,
+                      float epsilon,
+                      const engine& aengine = engine::cpu_engine()) {
+  tensor diff_scale_shift;
+  compute(src, mean, variance, diff_dst, scale, diff_src, diff_scale_shift, epsilon, aengine);
+  diff_scale.reinit_if_necessary(scale.get_desc());
+  diff_shift.reinit_if_necessary(scale.get_desc());
+  std::memcpy(diff_scale.get_data_handle(), (char*)diff_scale_shift.get_data_handle(), diff_scale.get_size());
+  std::memcpy(diff_shift.get_data_handle(),
+      (char*)diff_scale_shift.get_data_handle() + diff_scale.get_size(), diff_shift.get_size());
+  }
 };
 
 }  // namespace ideep
