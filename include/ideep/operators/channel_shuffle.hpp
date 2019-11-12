a81f0bef325017a0957d2a58a1567fb339810e8e
pinzhenx <pinzhen.xu@intel.com>
2019-11-12 15:14:17 +0800

    fix shuffle backward

--- include/ideep/operators/channel_shuffle.hpp
+++ include/ideep/operators/channel_shuffle.hpp
@@ -40,10 +40,11 @@ struct channel_shuffle_backward : public dnnl::shuffle_backward {
                       const int axis = 1,
                       const engine& aengine = engine::cpu_engine()) {
     auto group_size = diff_dst.get_dim(axis) / group;
+    auto data_desc = diff_dst.get_desc();
     auto forward_hints = dnnl::shuffle_forward::primitive_desc(
-        {prop_kind::forward, diff_src.get_desc(), group_size, axis}, aengine);
-    auto pd = primitive_desc({diff_dst.get_desc(), axis, group_size}, aengine,
-                             forward_hints);
+        {prop_kind::forward, data_desc, group_size, axis}, aengine);
+    auto pd =
+        primitive_desc({data_desc, axis, group_size}, aengine, forward_hints);
     auto expected_diff_dst = diff_dst.reorder_if_differ_in(pd.diff_dst_desc());
     diff_src.reinit_if_necessary(pd.diff_src_desc());
     super(pd).execute(stream::default_stream(),
