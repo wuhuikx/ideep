6c0ad584cd3bae49cfdf17879058883546d7da59
xiaobing.zhang <xiaobing.zhang@intel.com>
2019-11-18 10:28:42 +0800

    Add layer_norm forward

--- include/ideep/operators/layernorm.hpp
+++ include/ideep/operators/layernorm.hpp
@@ -4,7 +4,34 @@
 namespace ideep {
 
 struct layer_normalization_forward : public dnnl::layer_normalization_forward {
-  static inline void compute() {
+  using super = dnnl::layer_normalization_forward;
+  static void compute(const tensor& src,
+                      const tensor& scale,
+                      const tensor& shift,
+                      tensor& dst,
+                      tensor& mean,
+                      tensor& variance,
+                      float epsilon,
+                      const engine& aengine = engine::cpu_engine()) {
+    auto flags = batch_normalization_flag::use_scale_shift;
+    auto src_desc = src.get_desc();
+    auto pd = primitive_desc(
+        {prop_kind::forward_training, src_desc, epsilon, flags}, aengine);
+    tensor scale_shift {pd.weights_desc()};
+    std::memcpy(scale_shift.get_data_handle(),
+              scale.get_data_handle(), scale.get_size());
+    std::memcpy(scale_shift.get_data_handle() + scale.get_size(),
+                shift.get_data_handle(), shift.get_size());
+    auto expected_src = src.reorder_if_differ_in(pd.src_desc());
+    mean.reinit_if_necessary(pd.mean_desc());
+    variance.reinit_if_necessary(pd.variance_desc());
+    dst.reinit_if_necessary(pd.dst_desc());
+    super(pd).execute(stream::default_stream(),
+            {{DNNL_ARG_SRC, expected_src},
+            {DNNL_ARG_SCALE_SHIFT, scale_shift},
+            {DNNL_ARG_MEAN, mean},
+            {DNNL_ARG_VARIANCE, variance},
+            {DNNL_ARG_DST, dst}});
   }
 };
 
