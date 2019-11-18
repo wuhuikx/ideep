928c8e959265f8c16ac93f87d7016e229c85da62
pinzhenx <pinzhen.xu@intel.com>
2019-11-18 20:10:08 +0800

    optimize tensor format for pooling

--- include/ideep/operators/pool.hpp
+++ include/ideep/operators/pool.hpp
@@ -4,7 +4,9 @@
 namespace ideep {
 
 struct pooling_forward : public dnnl::pooling_forward {
+
   using super = dnnl::pooling_forward;
+
   static void compute(const tensor& src,
                       const dims& output_sizes,
                       tensor& dst,
@@ -29,17 +31,21 @@ struct pooling_forward : public dnnl::pooling_forward {
 
     auto expected_src = src.reorder_if_differ_in(pd.src_desc());
     dst.reinit_if_necessary(pd.dst_desc());
+
     exec_args args {{DNNL_ARG_SRC, expected_src}, {DNNL_ARG_DST, dst}};
     if (with_workspace) {
       dst.init_workspace(pd.workspace_desc());
       args.insert({DNNL_ARG_WORKSPACE, dst.get_workspace()});
     }
+
     super(pd).execute(stream::default_stream(), args);
   }
 };
 
 struct pooling_backward : public dnnl::pooling_backward {
+
   using super = dnnl::pooling_backward;
+
   static void compute(const tensor& diff_dst,
                       const tensor& dst,
                       const tensor& src,
@@ -50,24 +56,29 @@ struct pooling_backward : public dnnl::pooling_backward {
                       const dims& padding_r,
                       algorithm aalgorithm,
                       const engine& aengine = engine::cpu_engine()) {
-    auto src_desc = src.get_desc();
+    auto src_desc = src.get_desc().to_format_any();
     auto dst_desc = dst.get_desc();
-    auto forward_hints = pooling_forward::primitive_desc(
-        {prop_kind::forward, aalgorithm, src_desc, dst_desc, strides, kernel,
-         padding_l, padding_r}, aengine);
+
+    auto forward_hints =
+        pooling_forward::primitive_desc(
+            {prop_kind::forward, aalgorithm, src_desc, dst_desc, strides,
+             kernel, padding_l, padding_r}, aengine);
+
     auto pd = primitive_desc(
         {aalgorithm, src_desc, dst_desc, strides, kernel, padding_l, padding_r},
         aengine, forward_hints);
+
     auto expected_diff_dst = diff_dst.reorder_if_differ_in(pd.diff_dst_desc());
     diff_src.reinit_if_necessary(pd.diff_src_desc());
+
     exec_args args {{DNNL_ARG_DIFF_DST, expected_diff_dst},
                     {DNNL_ARG_DIFF_SRC, diff_src}};
-
     if (dst.has_workspace()) {
       auto expected_workspace =
           dst.get_workspace().reorder_if_differ_in(pd.workspace_desc());
       args.insert({DNNL_ARG_WORKSPACE, expected_workspace});
     }
+
     super(pd).execute(stream::default_stream(), args);
   }
 };
