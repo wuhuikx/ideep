6690c32e15bfb193651820cfdee8ace0904ff304
pinzhenx <pinzhen.xu@intel.com>
2019-12-05 13:32:03 +0800

    refactor bn

--- include/ideep/operators/batchnorm.hpp
+++ include/ideep/operators/batchnorm.hpp
@@ -9,35 +9,15 @@ struct batch_normalization_forward_inference
 
   using super = dnnl::batch_normalization_forward;
 
-  // XPZ: TODO: fold these two overloads
   static void compute(const tensor& src,
                       const tensor& scale,
                       const tensor& shift,
                       tensor& dst,
                       float epsilon,
                       const engine& aengine = engine::cpu_engine()) {
-    // XPZ: this overload has not been tested
-    auto flags = batch_normalization_flag::use_scale_shift;
-
-    // workaround: use src.get_desc() once issue intel/mkl-dnn#588 is resolved
-    auto src_desc = src._get_unblocked_desc_if_4c_blocked();
-    // auto src_desc = src.get_desc();
-
-    auto pd = primitive_desc(
-        {prop_kind::forward_inference, src_desc, epsilon, flags}, aengine);
-
-    tensor scale_shift {pd.weights_desc()};
-    std::memcpy(scale_shift.get_data_handle(),
-                scale.get_data_handle(), scale.get_size());
-    std::memcpy(scale_shift.get_data_handle() + scale.get_size(),
-                shift.get_data_handle(), shift.get_size());
-    auto expected_src = src.reorder_if_differ_in(pd.src_desc());
-    dst.reinit_if_necessary(pd.dst_desc());
-
-    super(pd).execute(stream::default_stream(),
-                      {{DNNL_ARG_SRC, expected_src},
-                       {DNNL_ARG_SCALE_SHIFT, scale_shift},
-                       {DNNL_ARG_DST, dst}});
+    static tensor dummy;
+    compute_impl</*use_stats=*/false>(
+        src, dummy, dummy, scale, shift, dst, epsilon, aengine);
   }
 
   static void compute(const tensor& src,
@@ -48,8 +28,22 @@ struct batch_normalization_forward_inference
                       tensor& dst,
                       float epsilon,
                       const engine& aengine = engine::cpu_engine()) {
-    auto flags = batch_normalization_flag::use_scale_shift |
-                 batch_normalization_flag::use_global_stats;
+    compute_impl</*use_stats=*/true>(
+        src, mean, variance, scale, shift, dst, epsilon, aengine);
+  }
+ private:
+  template <bool use_stats>
+  static void compute_impl(const tensor& src,
+                           const tensor& mean,
+                           const tensor& variance,
+                           const tensor& scale,
+                           const tensor& shift,
+                           tensor& dst,
+                           float epsilon,
+                           const engine& aengine) {
+    auto flags = batch_normalization_flag::use_scale_shift;
+    if (use_stats)
+      flags |= batch_normalization_flag::use_global_stats;
 
     // workaround: use src.get_desc() once issue intel/mkl-dnn#588 is resolved
     auto src_desc = src._get_unblocked_desc_if_4c_blocked();
@@ -64,16 +58,23 @@ struct batch_normalization_forward_inference
     std::memcpy(scale_shift.get_data_handle() + scale.get_size(),
                 shift.get_data_handle(), shift.get_size());
     auto expected_src = src.reorder_if_differ_in(pd.src_desc());
-    auto expected_mean = mean.reorder_if_differ_in(pd.mean_desc());
-    auto expected_variance = variance.reorder_if_differ_in(pd.variance_desc());
     dst.reinit_if_necessary(pd.dst_desc());
 
-    super(pd).execute(stream::default_stream(),
-                      {{DNNL_ARG_SRC, expected_src},
-                       {DNNL_ARG_SCALE_SHIFT, scale_shift},
-                       {DNNL_ARG_VARIANCE, expected_variance},
-                       {DNNL_ARG_MEAN, expected_mean},
-                       {DNNL_ARG_DST, dst}});
+    if (use_stats) {
+      auto expected_mean = mean.reorder_if_differ_in(pd.mean_desc());
+      auto expected_var = variance.reorder_if_differ_in(pd.variance_desc());
+      super(pd).execute(stream::default_stream(),
+                        {{DNNL_ARG_SRC, expected_src},
+                         {DNNL_ARG_SCALE_SHIFT, scale_shift},
+                         {DNNL_ARG_VARIANCE, expected_var},
+                         {DNNL_ARG_MEAN, expected_mean},
+                         {DNNL_ARG_DST, dst}});
+    } else {
+      super(pd).execute(stream::default_stream(),
+                        {{DNNL_ARG_SRC, expected_src},
+                         {DNNL_ARG_SCALE_SHIFT, scale_shift},
+                         {DNNL_ARG_DST, dst}});
+    }
   }
 };
 
@@ -99,6 +100,7 @@ struct batch_normalization_forward_training
 
     auto pd = primitive_desc(
         {prop_kind::forward_training, src_desc, epsilon, flags}, aengine);
+
     tensor scale_shift {pd.weights_desc()};
     std::memcpy(scale_shift.get_data_handle(),
                 scale.get_data_handle(), scale.get_size());
@@ -108,6 +110,7 @@ struct batch_normalization_forward_training
     mean.reinit_if_necessary(pd.mean_desc());
     variance.reinit_if_necessary(pd.variance_desc());
     dst.reinit_if_necessary(pd.dst_desc());
+
     super(pd).execute(stream::default_stream(),
                       {{DNNL_ARG_SRC, expected_src},
                        {DNNL_ARG_SCALE_SHIFT, scale_shift},
